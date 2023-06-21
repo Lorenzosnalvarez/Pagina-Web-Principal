14520aa6743d6948df30015c31c9e4d7c17f01b9
Lorenzo.Alvarez <loralvarez@itba.edu.ar>
2023-06-21 16:26:14 -0300

    d

---
+++ myscript.js
@@ -0,0 +1,10 @@
+<button onclick="window.print()">Print this page</button>
+//esto no va a ser ejecutado 
+/*comentario
+ola */
+
+
+    function todos() 
+    {document.getElementById("demo").innerHTML = "Grid con todos los productos";}
+    function quimicos() 
+    {document.getElementById("demo").innerHTML = "Grid con todos los quimicos";}
