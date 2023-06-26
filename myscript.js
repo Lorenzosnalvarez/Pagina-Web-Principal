8dafe1266725cde231312cfd9d4fa57df1dae661
Lorenzo.Alvarez <loralvarez@itba.edu.ar>
2023-06-26 16:21:16 -0300

    f

--- myscript.js
+++ myscript.js
@@ -1,5 +1,24 @@
+var diasElement = document.getElementById("dias");
+var horasElement = document.getElementById("horas");
+var minutosElement = document.getElementById("minutos");
+var segundosElement = document.getElementById("segundos");
 
-//esto no va a ser ejecutado 
-/*comentario
-ola */
+var fechaLimite = new Date("2023-06-27T00:00:00");
+
+function actualizarContador() {
+  var ahora = new Date();
+  var diferencia = fechaLimite - ahora;
+
+  var dias = Math.floor(diferencia / (1000 * 60 * 60 * 24));
+  var horas = Math.floor((diferencia % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutos = Math.floor((diferencia % (1000 * 60 * 60)) / (1000 * 60));
+  var segundos = Math.floor((diferencia % (1000 * 60)) / 1000);
+  
+  diasElement.textContent = dias.toString();
+  horasElement.textContent = horas.toString();
+  minutosElement.textContent = minutos.toString();
+  segundosElement.textContent = segundos.toString();
+}
+actualizarContador();
+setInterval(actualizarContador, 1000);
 
